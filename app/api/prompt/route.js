f8c372d8e76a3f7101df5b0eae20d71140ec9498
Rahul <dadarahulshaik@gmail.com>
2024-10-17 17:49:52 -0700

    cache fix

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,7 +5,16 @@ export const GET = async (req) => {
   try {
     await connectToDB();
     let prompts = await Prompt.find({}).populate("creator");
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    // Set headers to avoid caching
+    const headers = new Headers({
+      "Content-Type": "application/json",
+      "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+      Pragma: "no-cache",
+      Expires: "0",
+      "Surrogate-Control": "no-store",
+    });
+
+    return new Response(JSON.stringify(prompts), { status: 200, headers });
   } catch (error) {
     return new Response("unable to get all the prompts", { status: 500 });
   }
